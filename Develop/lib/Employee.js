30d2f17e6095881c23f452438a422734a050b8f4
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-18 17:13:11 -0500

    Added skeletons for all classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor (name, id, email) {
+        this.name = name,
+        this.id = id,
+        this.email = email
+    }
+
+    getName() {
+        console.log("whee")
+    }
+
+    getID() {
+        console.log("whee1")
+    }
+
+    getEmail() {
+        console.log("whee2")
+    }
+
+    getRole() {
+        return "Employee"
+    }
+
+}
+
+module.exports = Employee;
