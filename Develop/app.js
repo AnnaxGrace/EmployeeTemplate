0dce7149219d96d07d0ad5c51ea19742b8996c1a
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-21 17:28:44 -0500

    Added readme

--- Develop/app.js
+++ Develop/app.js
@@ -10,8 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// const html = require("./lib/htmlRenderer");
-
 var objectArray = [];
 
 inquirer.prompt ([
@@ -104,9 +102,7 @@ inquirer.prompt ([
                 const newEngineer = new Engineer(responses.engineerName, responses.engineerID, responses.engineerEmail, responses.github);
                 console.log(newEngineer);
                 objectArray.push(newEngineer);
-                // var userName = "github.com/" + responses.github
                 if (responses.employeeType === "Engineer") {
-                    // var newEngineer = new Engineer(responses.engineerName, responses.engineerID, responses.engineerEmail, responses.github)
                     ifEngineer();
                 }
                 else if (responses.employeeType === "Intern") {
@@ -173,7 +169,6 @@ inquirer.prompt ([
     }
 
 function makeHTML() {
-    // render(objectArray);
     const html = render(objectArray)
         console.log("why no work?")
                 
@@ -182,34 +177,3 @@ function makeHTML() {
         if (err) throw err
         });
 }
-// render(objectArray);
-
-// var newHTML = render.data
-// console.log(render.data);
-
-// fs.writeFile(outputPath, newHTML, function(err) {
-//     if (err) throw err;
-// });
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# EmployeeTemplate
+[![made-for-VSCode](https://img.shields.io/badge/Made%20for-VSCode-1f425f.svg)](https://code.visualstudio.com/)
+# Employee Template
+## Description
+This application uses prompts to create your employee team that consists of a manager, engineers, and interns.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+### Installation
+npm i
+### Usage
+You run this application in terminal or bash with node app.js in the Develop folder. After following the prompts, an html titled team.html will populate in the Output folder.
+### License
+MIT
+### Contributing
+Contributors follow contributors convenant after July 2020. Before then no contributors are allowed.
+### Tests
+npm tests
+### Questions
+* Github username: annaxgrace
+* Email: anna.grace.conover@gmail.com
+* GitHub repository URL: https://github.com/AnnaxGrace/EmployeeTemplate
+
+<img src="https://avatars2.githubusercontent.com/u/61895571?s=460&u=51c1a5027c2ee815104a12b2aded2bcf51b2c6b7&v=4"
+     alt="Contributer Photo"
+     style="float: left; margin-right: 10px;" />
+    
